3abfaf040aa820c2a3591bc48879106bb16597f2
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2020-12-21 21:16:50 +0100

    Create Test_positivité.py

---
+++ Test_positivité.py
@@ -0,0 +1,7 @@
+x=int(input("Saisir x : ")) #Test de positivité version 2. 
+if x>0 :
+    print ("x est strictement positif.")
+elif x==0 :
+    print ("x est nul.")
+else :
+    print ("x est strictement négatif.")
